7a4ed3f8c05dc2b14f7c7eb35d8ad8c7cfd310eb
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-08-16 21:15:53 -0300

    Changed variable app to server

--- src/api/app.ts
+++ src/api/app.ts
@@ -2,25 +2,25 @@ import express, { Express } from 'express'
 import routes from './routes'
 
 class App {
-  private app: Express
+  private server: Express
 
   constructor () {
-    this.app = express()
+    this.server = express()
 
     this.setupMiddlewares()
     this.setupRoutes()
   }
 
-  public getApp () : Express {
-    return this.app
+  public getServer () : Express {
+    return this.server
   }
 
   private setupMiddlewares () : void {
-    this.app.use(express.json())
+    this.server.use(express.json())
   }
 
   private setupRoutes () : void {
-    this.app.use('/api', routes)
+    this.server.use('/api', routes)
   }
 }
 
