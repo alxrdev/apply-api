518ac93044da5d1d9d5bbfbddfbf2b8458ae83b2
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-08-27 19:18:23 -0300

    Removed deprectade classes

--- src/modules/jobs/entities/index.ts
+++ src/modules/jobs/entities/index.ts
@@ -1,9 +1,5 @@
 import Job from './Job'
 import Address from './Address'
-import Industry from './Industry'
-import JobType from './JobType'
-import Education from './Education'
-import Experience from './Experience'
 import JobResponse from './JobResponse'
 import CollectionResponse from './CollectionResponse'
 import FilesToDeleteCollection from './FilesToDeleteCollection'
@@ -12,10 +8,6 @@ import FileToDelete from './FileToDelete'
 export {
   Job,
   Address,
-  Industry,
-  JobType,
-  Education,
-  Experience,
   JobResponse,
   CollectionResponse,
   FilesToDeleteCollection,
