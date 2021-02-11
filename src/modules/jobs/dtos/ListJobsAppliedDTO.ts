3f75b0fb72286e2fa03100c236d0703e60a0e369
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-02-11 20:40:48 -0300

    Tests the ListJobsAppliedUseCase

--- src/modules/jobs/dtos/ListJobsAppliedDTO.ts
+++ src/modules/jobs/dtos/ListJobsAppliedDTO.ts
@@ -1,14 +1,16 @@
-import { IsDefined, IsString } from 'class-validator'
+import { IsDefined, IsNotEmpty, IsString } from 'class-validator'
 import { Expose } from 'class-transformer'
 
 export default class ListJobsAppliedDTO {
   @IsDefined()
   @IsString()
+  @IsNotEmpty()
   @Expose()
   id!: string
 
   @IsDefined()
   @IsString()
+  @IsNotEmpty()
   @Expose()
   authId!: string
 }
