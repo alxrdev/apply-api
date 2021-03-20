51c02cc6033c6dce68a765251f20a736d2335f35
Alex Rodrigues <rodriguesalex793@gmail.com>
2021-03-20 17:01:23 -0300

    Method name has been changed

--- tests/auth.ts
+++ tests/auth.ts
@@ -3,7 +3,7 @@ import { container } from 'tsyringe'
 import { User } from '@modules/users/entities'
 import ITokenBasedAuthService from '@services/auth/interfaces/ITokenBasedAuthService'
 
-export const getToken = (id: string, role: string) : string => {
+export const generateToken = (id: string, role: string) : string => {
   const authService : ITokenBasedAuthService = container.resolve('AuthService')
   return authService.generateToken({ id, role } as User)
 }
